412eff922b4590c8782d13e76fdfd4f5c4f211be
josecapewa <capewajose@gmail.com>
2024-02-25 23:36:15 +0100

    Adding attacking animations + state machines

--- Player/Player.cs
+++ Player/Player.cs
@@ -7,6 +7,13 @@ public partial class Player : CharacterBody2D
 	const float MAX_SPEED = 80;
 	const float FRICTION = 500;
 	
+	enum movimento{
+		MOVE,
+		ROLL,
+		ATTACK
+	}
+	
+	int state = (int)movimento.MOVE;
 	Vector2 velocity = Vector2.Zero;
 	
 	AnimationPlayer animationPlayer;
@@ -21,6 +28,19 @@ public partial class Player : CharacterBody2D
 		
 	public override void _PhysicsProcess(double delta)
 	{
+		switch(state){
+			case 0:
+				moveState(delta);
+				break;
+			case 1:
+				break;
+			case 2:
+				attackState(delta);
+				break;
+		}
+	}
+	
+	public void moveState(double delta){
 		var input = Vector2.Zero;
 		input.X = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
 		input.Y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
@@ -29,14 +49,27 @@ public partial class Player : CharacterBody2D
 		if(input!=Vector2.Zero){
 			animationTree.Set("parameters/Idle/blend_position", input);
 			animationTree.Set("parameters/Run/blend_position", input);
+			animationTree.Set("parameters/Attack/blend_position", input);
 			animationState.Travel("Run");
 			velocity = velocity.MoveToward(input * MAX_SPEED, ACCELERATION * (float)delta);
 		} else{
 			animationState.Travel("Idle");
 			velocity = velocity.MoveToward(Vector2.Zero, FRICTION * (float)delta);
 		}
-		
 		Velocity = velocity;
 		MoveAndSlide();
+		
+		if(Input.IsActionJustPressed("attack")){
+			state = (int)movimento.ATTACK;
+		}
+	}
+	
+	public void attackState(double delta){
+		velocity = Vector2.Zero;
+		animationState.Travel("Attack");
+	}
+	
+	public void attackAnimationFinished(){
+		state = (int)movimento.MOVE;
 	}
 }
